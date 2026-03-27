4f16d6b301893964dfb72f8f62a789d7da3a01a9
Sry kimsour <soursour3001@gmail.com>
2026-03-27 11:32:50 +0700

    Update

--- WEEK9/service/music_service.dart
+++ WEEK9/service/music_service.dart
@@ -13,19 +13,15 @@ class MusicService {
   MusicService({required this.songRepository, required this.artistRepository});
 
   Future<List<SongDetails>> fetchSongDetails() async {
-    // 1- Fetch  all song
     List<Song> songs = await songRepository.fetchSongs();
 
-    // 2- Fethc artist
     List<Artist> artists = await artistRepository.fetchArtists();
 
-    // 3- Create the mapping artistid-> artist
     Map<String, Artist> mapArtist = {};
     for (Artist artist in artists) {
       mapArtist[artist.id] = artist;
     }
 
-    // 4 - Return  the mapped data
     return songs
         .map(
           (song) => SongDetails(song: song, artist: mapArtist[song.artistId]!),
